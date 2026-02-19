6dee261c83dbdaba05d54bc959bf0d745c73045d
Mani rathna kumar.V <rathnakumar30062006@gmail.com>
2026-02-19 22:12:13 +0530

    Add files via upload

---
+++ if.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+void main() {
+    int a=5,b=10;
+    if(a<b)
+    {
+        printf("manirathnakumar");
+    }
+        else
+        {
+            printf("naveen");
+        }
+    
+    getch();
+}
